e273e7945064059127f6ef72d14777233a5b1e8e
He Fei <droxer.he@gmail.com>
2016-09-21 22:43:24 +0800

    it works.

--- ingestor/ingestor.py
+++ ingestor/ingestor.py
@@ -1,8 +1,22 @@
+import json
+
 from kafka import KafkaConsumer
 
 
 def main():
-    pass
+    consumer = KafkaConsumer('roomis', bootstrap_servers='192.168.99.100:9092')
+    for msg in consumer:
+        dispatch(msg)
+
+def _extract(msg):
+    value = str(msg.value, 'utf-8')
+    return json.loads(value)
+
+def dispatch(msg):
+    changes = _extract(msg)
+    print("Update: table = {}".format(changes['table']))
+    print("Update: type = {}".format(changes['type']))
+    print("Update: data = {}".format(changes['data']))
 
 if __name__ == '__main__':
     main()
